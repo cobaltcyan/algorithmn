d45e4bef6b1b593a5cdfa78e6c13d5326f74048a
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-07 00:59:13 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let answer = 0;
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + parseInt(arr[i]);
+    }
+    
+    answer = sum / arr.length;
+    
+    return answer;
+}
