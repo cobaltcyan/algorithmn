3d6ffd7a3e62fc4c5bb8a2c84f10ef243be7867f
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-07 02:48:49 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
@@ -1,9 +1,12 @@
 function solution(age) {
-    var answer = 0;
+    let answer = 0;
     const year = 2022;
     
     if (0 <= age <= 120) {
         answer = year - age + 1;
     }
     return answer;
+    
+    // return new Date().getFullYear() - age + 1;
+    
 }
