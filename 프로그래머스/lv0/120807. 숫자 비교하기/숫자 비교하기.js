f7937d4845beddaa75a5736cb1359115d83e5e2e
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-07 02:38:23 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+    let answer = 0;
+    if (0 <= num1 <= 10000 && 0 <= num2 <= 10000) {
+        if (num1 === num2)
+            answer = 1;
+        else
+            answer = -1;
+    }
+    return answer;
+}
