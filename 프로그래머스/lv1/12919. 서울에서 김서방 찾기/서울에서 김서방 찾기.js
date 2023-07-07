b23a0221f0a0994989d4f31fe3f7f674d735e2a2
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-08 02:48:40 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,4 +1,18 @@
 function solution(seoul) {
-    var answer = '';
+    let answer = '';
+    
+    // if (1 <= seoul.length <= 1000) {
+    //     for (let i = 0; i < seoul.length; i++) {
+    //         if (1 <= seoul[i].length <= 20) {
+    //             if (seoul[i] === "Kim") {
+    //                 answer = "김서방은 " + i + "에 있다";   
+    //             }   
+    //         }
+    //     }   
+    // }
+
+    let idx = seoul.indexOf('Kim');
+    answer = "김서방은 " + idx + "에 있다";
+
     return answer;
 }
