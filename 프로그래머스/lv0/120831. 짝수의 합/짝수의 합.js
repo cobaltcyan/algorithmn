df10ab36021ebde800c525b589934a4da35b34b2
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-08 20:47:22 +0900

    [level 0] Title: 짝수의 합, Time: 2.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -10,4 +10,20 @@ function solution(n) {
     answer = sum;
     console.log(answer);
     return answer;
+    
+    // 1.
+    // var half = Math.floor(n/2);
+    // return half*(half+1);
+
+    // 2.
+    for(let i=2 ; i<=n ; i+=2)
+        answer += i;
+
+    // 3.
+    // Array(n)
+    //     .fill()
+    //     .map((_, i) => i + 1)
+    //     .filter((v) => v % 2 === 0)
+    //     .reduce((acc, cur) => acc + cur, 0);
+    
 }
