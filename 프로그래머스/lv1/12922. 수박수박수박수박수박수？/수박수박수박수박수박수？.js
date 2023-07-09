5d12ead40031495a6a2b661060ab84b0d0688b01
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-09 22:00:36 +0900

    Update 수박수박수박수박수박수？.js

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -15,10 +15,13 @@ function solution(n) {
     }
     answer = output;
     return answer;
+   
 }
-
 /*
     여기서 "길이 10,000 이하인 자연수"는 일반적으로 숫자의 자릿수를 의미하는 것이 아닙니다.
     대신, n은 자연수를 나타내는 변수이며, 그 값은 10,000 이하여야 한다는 것입니다.
     즉, n은 1 이상 10,000 이하의 양의 정수를 가질 수 있다는 의미입니다.
 */
+
+// const waterMelon = n => "수박".repeat(n).slice(0,n);
+
