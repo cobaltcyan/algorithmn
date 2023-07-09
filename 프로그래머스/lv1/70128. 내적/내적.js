e0205ae93832e6a547f1faf6d5b0c3de8fe053b9
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-09 22:25:01 +0900

    Update 내적.js

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -7,3 +7,15 @@ function solution(a, b) {
     answer = sum;
     return answer;
 }
+/*
+    return (a,b)=>a.reduce((a,c,i) => a+c*b[i], 0);
+    
+    a.reduce((a, c, i) => a + c * b[i], 0): a 배열의 reduce() 메서드를 호출하여 누적 합산을 수행
+    
+    a: 이전 누적 값
+    c: 현재 배열 요소
+    i: 현재 인덱스
+    b[i]: b 배열의 현재 인덱스에 해당하는 요소
+    a + c * b[i]: 이전 누적 값에 c와 b[i]를 곱한 값을 더합니다.
+    0: 초기 누적 값으로 0을 설정합니다.
+*/
