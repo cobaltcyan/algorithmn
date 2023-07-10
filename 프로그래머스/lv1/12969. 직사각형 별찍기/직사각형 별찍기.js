c02f0abd84c6c8d50594fa5437a2877322faf3a7
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-10 23:30:48 +0900

    Update 직사각형 별찍기.js

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,10 +2,15 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-
-    for(let row=0;row<b;row++){
-        console.log('*'.repeat(a))
+    
+    const row = '*'.repeat(a);
+    for(let i = 0; i < b; i++){
+        console.log(row);
     }
+    
+    // for(let row = 0; row < b; row++){
+    //     console.log('*'.repeat(a));
+    // }
 });
 
 // process.stdin.setEncoding('utf8');
@@ -17,6 +22,8 @@ process.stdin.on('data', data => {
 // });
 
 /*
+    // 문제코드 해석
+    
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', data => {
         const n = data.split(" ");
@@ -41,4 +48,16 @@ process.stdin.on('data', data => {
     
     따라서 위의 코드는 사용자로부터 두 개의 숫자를 공백으로 구분하여 입력받고,
     입력된 숫자를 변수 a와 b에 저장하여 출력하는 기능을 수행합니다.
+*/
+/*
+    // repeat() 함수 : 문자열을 반복하여 새로운 문자열을 생성하는 JS 내장함수
+    // string.repeat(count);
+    string : 반복할 대상이 되는 문자열
+    count : 반복횟수를 나타내는 양의 정수 값
+    repeat() 은 string 을 count 만큼 반복하여 새로운 문자열 생성함.
+    abc.repeat(3) -> "abcabcabc"
+    count 는 0 또는 음수가 될 수 없으며, 소수값은 정수로 변환됌. 2.5 -> 2
+    ES6(ECMAScript2015)에서 추가된 기능이므로 호환성을 고려하여 사용할것.
+    
+
 */
