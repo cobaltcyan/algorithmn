dce6f6ecceaba192a2a3b2cff96e17c92b5aebfa
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-10 23:11:04 +0900

    Update 문자열 다루기 기본.js

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,30 +1,39 @@
 function solution(s) {
-s = s.split('').map(Number);
-    if(s.length === 4 || s.length=== 6){
-     if(!s.includes(NaN)){
-       return true
-       } else{
-           return false
-       }
-    }else {
-        return false
+    let answer = true;
+    const regex = /^[0-9]+$/;
+    if (s.length === 4 || s.length === 6) {
+        if (regex.test(s)) {
+            answer = true;
+        } else {
+            answer = false;
+        }    
+    } else {
+        answer = false;
     }
+    console.log(answer);
+    return answer;
 }
+/*
+    // 정규표현식
+    
+    // pattern ^시작 $ 끝 + 정의한 표현식 하나 이상
+    
+    // const regex = /^[a-zA-Z0-9]$/;
+    // regex.test(테스트할문자열);
+    
+*/
 
-
-// function solution(s) {
-//     let answer = true;
-//     // const regex = /^[a-zA-Z0-9]$/;
-//     const regex = /^[0-9]%/;
-//     console.log(s);
-//     console.log(regex.test(s));
-//     if (s.length >= 1 && s.length <= 8) {
-//         if (regex.test(s) === true) {
-//             answer = true;
-//         } else {
-//             answer = false;
-//         }    
-//     }
-//     console.log(answer);
-//     return answer;
-// }
+/*
+    function solution(s) {
+        s = s.split('').map(Number);
+        if(s.length === 4 || s.length=== 6){
+         if(!s.includes(NaN)){
+           return true
+           } else{
+               return false
+           }
+        } else {
+            return false
+        }
+    }
+*/
