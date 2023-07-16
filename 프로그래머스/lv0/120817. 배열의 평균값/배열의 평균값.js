87f73aae08660974dc1d406d11714ed962e9beb2
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-16 15:14:50 +0900

    Update 배열의 평균값.js

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,9 +1,27 @@
 function solution(numbers) {
     let answer = 0;
+    answer = numbers.reduce((acc,cur) => acc + cur, 0) / numbers.length;
+    return answer;
+}
+
+/*
+function solution(numbers) {
+    var answer = 0;
+    for(i of numbers) {
+        answer += i
+    }
+    return answer / numbers.length;
+
+}
+*/
+/*
+function solution(numbers) {
+    let answer = 0;    
     answer = numbers.reduce(function(acc,cur) {
         return acc += cur;
     }, 0);
     answer = parseFloat(answer / numbers.length);
-    console.log(answer);
     return answer;
 }
+
+*/
