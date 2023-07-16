62b62af811aa0b79de5b37edfa31fc6dd2e40ab8
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2023-07-16 16:04:37 +0900

    Update 배열 뒤집기.js

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,9 +1,11 @@
 function solution(num_list) {
     let answer = [];
     
-    for (let i = num_list.length - 1; i >= 0 ; i--) {
-        answer.push(num_list[i]);
-    }
+//     for (let i = num_list.length - 1; i >= 0 ; i--) {
+//         answer.push(num_list[i]);
+//     }
+    
+    answer = num_list.reverse();
     console.log(answer);
     return answer;
 }
