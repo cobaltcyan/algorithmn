b288b3fa44bf79ddf542ea12384ede02928f385c
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2024-01-17 00:00:17 +0900

    Update 점의 위치 구하기.kt

--- 프로그래머스/Lv.0/120841. 점의 위치 구하기/점의 위치 구하기.kt
+++ 프로그래머스/Lv.0/120841. 점의 위치 구하기/점의 위치 구하기.kt
@@ -1,20 +1,28 @@
 class Solution {
     fun solution(dot: IntArray): Int {
-        var answer: Int = 0
-        
-        if (dot[0] > 0 && dot[1] > 0) {
-            answer = 1;
+        return when {
+            dot[0] > 0 -> if (dot[1] > 0) 1 else 4
+            dot[0] < 0 -> if (dot[1] > 0) 2 else 3
+            else -> 0 // (0, 0)일 경우
         }
-        else if(dot[0] < 0 && dot[1] > 0) {
-            answer = 2;
-        }
-        else if(dot[0] < 0 && dot[1] < 0) {
-            answer = 3;
-        }
-        else if(dot[0] > 0 && dot[1] < 0) {
-            answer = 4;
-        }
-        
-        return answer
     }
 }
+
+// class Solution {
+//     fun solution(dot: IntArray): Int {
+//         var answer: Int = 0
+//         if (dot[0] > 0 && dot[1] > 0) {
+//             answer = 1;
+//         }
+//         else if(dot[0] < 0 && dot[1] > 0) {
+//             answer = 2;
+//         }
+//         else if(dot[0] < 0 && dot[1] < 0) {
+//             answer = 3;
+//         }
+//         else if(dot[0] > 0 && dot[1] < 0) {
+//             answer = 4;
+//         }
+//         return answer
+//     }
+// }
