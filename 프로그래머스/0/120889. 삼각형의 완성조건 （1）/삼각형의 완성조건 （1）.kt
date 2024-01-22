db28229248661df264f2e3a7dcaa9c8e8dd3f428
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2024-01-23 02:16:00 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.32 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.kt
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    fun solution(sides: IntArray): Int {
+        var answer: Int = 0
+        
+        Arrays.sort(sides);
+        
+        if (sides[0] + sides[1] <= sides[2]) {
+            answer = 2
+        }
+        else
+            answer = 1
+        
+        return answer
+    }
+}
