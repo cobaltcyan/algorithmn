a4f6e8a30199d037d8bcdf1fe08f1157b3498d2a
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2024-01-23 17:20:43 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -11,3 +11,36 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제 풀이
+
+// import java.util.Arrays;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         // 테스트코드
+//         int[] num_list = {1, 2, 3, 4, 5};
+//         int[] result = solution(num_list);
+//         System.out.println(Arrays.toString(result));
+//     }
+
+//     public static int[] solution(int[] num_list) {
+//         int[] answer = new int[2]; // 결과를 저장할 배열, 크기는 2로 설정
+
+//         int num1 = 0;
+//         int num2 = 0;
+
+//         for (int i = 0; i < num_list.length; i++) {
+//             // System.out.println(num_list[i]);   // 1, 2, 3, 4, 5
+//             if (num_list[i] % 2 == 0)
+//                 num1++;
+//             else
+//                 num2++;
+//         }
+
+//         answer[0] = num1;
+//         answer[1] = num2;
+
+//         return answer;
+//     }
+// }
