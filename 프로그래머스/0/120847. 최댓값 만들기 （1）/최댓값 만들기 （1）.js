a2cf279fc90c7efb1ab7f3e54246457c4b5904b0
PARK SAEJIN <113007798+cobaltcyan@users.noreply.github.com>
2024-01-25 00:06:08 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 3.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,8 +1,18 @@
 function solution(numbers) {
     var answer = 0;
-    // return answer;
     numbers.sort((a,b) => a - b);
-    console.log(numbers);
     answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
     return answer;
 }
+
+// 내림차순
+// function solution(numbers) {
+//     numbers.sort((a, b) => b - a);
+//     return numbers[0]*numbers[1];
+// }
+
+// 구조분해할당 방법
+// function solution(numbers) {
+//     let [a, b] = numbers.sort((a,b) => b - a);
+//     return a * b;
+// }
